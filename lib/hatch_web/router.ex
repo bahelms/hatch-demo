fbe4b570014145621ea44f9a11b300f3e299917a
jimbonk <jimbonkgit@gmail.com>
2025-05-02 08:39:36 -0400

    Initial setup

---
+++ lib/hatch_web/router.ex
@@ -0,0 +1,10 @@
+defmodule HatchWeb.Router do
+  use HatchWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", HatchWeb do
+  end
+end
---
+++ test/hatch_test.exs
@@ -0,0 +1,7 @@
+defmodule HatchTest do
+  use ExUnit.Case
+  doctest Hatch
+
+  test "greets the world" do
+  end
+end
